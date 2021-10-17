f9fbd058b5d806bdb9eae00cb8b6d445831371a4
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-16 20:06:28 -0400

    pseudocode for exercise 42

---
+++ src/main/java/baseline/solution42.java
@@ -0,0 +1,18 @@
+package baseline;
+
+public class solution42 {
+
+    /*
+    Create method that prints details from file
+    takes name of the file name
+
+    create a scanner that scans the file
+
+    prints out the contents of the file using a while loop
+
+    using an exception, catch if the file name is not found
+
+    print out the final list of the names in tabular form.
+     */
+
+}
